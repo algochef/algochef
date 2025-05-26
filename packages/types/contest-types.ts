e102a20805b7e1dbaaa9ffae820d127fa56a64d8
terminalwarlord <joybiswas389@gmail.com>
2025-05-26 13:20:12 +0600

    added: types for contests

---
+++ packages/types/contest-types.ts
@@ -0,0 +1,21 @@
+export enum Platform {
+    CODEFORCES = "CODEFORCES",
+    LEETCODE = "LEETCODE",
+    CODECHEF = "CODECHEF",
+    ATCODER = "ATCODER"
+};
+
+
+
+export interface Contest{
+    id?: number;
+    title: string;
+    url: string;
+    youtubeUrl?: string;
+    platform: Platform;
+    startsAt: number;
+    duration: number;
+    hasEnded?: boolean;
+    isBookmarked?: boolean;
+    isRunning?: boolean;
+}
