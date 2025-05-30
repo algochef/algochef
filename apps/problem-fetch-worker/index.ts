a99bd9d956a708302aaa0146379474f2063152af
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2025-05-30 10:05:19 +0600

    (+)Completed CF and LC problem fetch

--- apps/problem-fetch-worker/index.ts
+++ apps/problem-fetch-worker/index.ts
@@ -1,8 +1,11 @@
 import { codeforcesProblems } from "./providers/codeforces";
+import { leetcodeProblems } from "./providers/leetcode";
 
 const updateProblems = async () => {
-  await codeforcesProblems();
-  console.log("Processed CodeForces Problems");
+  // await codeforcesProblems();
+  // console.log("Processed CodeForces Problems");
+  // await leetcodeProblems();
+  // console.log("Processed LeetCode Problems");
 };
 
 (async () => {
