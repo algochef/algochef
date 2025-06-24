4b1ba91e02850a2e39263274c6cf6215568ee978
terminalwarlord <joybiswas389@gmail.com>
2025-06-25 03:24:10 +0600

    test husky hook

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -5,5 +5,8 @@ const globalPrisma = global as unknown as { prisma: PrismaClient };
 export const prismaClient = globalPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
+
+
+  
   globalPrisma.prisma = prismaClient;
 }
